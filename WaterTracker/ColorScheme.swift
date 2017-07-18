2bc0976ea715f82dfad519e98f195f14788ed02d
Ryan Phillips <rphillips4417@gmail.com>
2017-07-18 15:10:28 -0600

    ColorScheme file set up. and initial set up of MainVC in progress

---
+++ WaterTracker/ColorScheme.swift
@@ -0,0 +1,29 @@
+//
+//  ColorScheme.swift
+//  WaterTracker
+//
+//  Created by Ryan Phillips on 7/18/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorScheme {
+    static let darkPrimaryColor = UIColor (red: 0.1, green: 0.46, blue: 0.82, alpha: 1.0)
+    static let defaultPrimaryColor = UIColor (red: 0.13, green: 0.59, blue: 0.95, alpha: 1.0)
+    static let lightPrimaryColor = UIColor (red: 0.73, green: 0.87, blue: 0.98, alpha: 1.0)
+        
+        
+        
+        
+//        .dark-primary-color    { background: #1976D2; }
+//            .default-primary-color { background: #2196F3; }
+//                .light-primary-color   { background: #BBDEFB; }
+//                    .text-primary-color    { color: #FFFFFF; }
+//                        .accent-color          { background: #009688; }
+//                            .primary-text-color    { color: #212121; }
+//                                .secondary-text-color  { color: #757575; }
+//                                    .divider-color         { border-color: #BDBDBD; }
+//}
+}
---
+++ WaterTracker/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  WaterTracker
+//
+//  Created by Ryan Phillips on 7/18/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    let backView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        
+    }
+
+
+}
+
