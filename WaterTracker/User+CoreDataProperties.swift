20213e2b12fba56baf782b3487a90952aefa0b1e
Ryan Phillips <rphillips4417@gmail.com>
2017-08-01 12:56:55 -0600

    Weight sort order finally working

--- WaterTracker/User+CoreDataProperties.swift
+++ WaterTracker/User+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  User+CoreDataProperties.swift
 //  WaterTracker
 //
-//  Created by Ryan Phillips on 7/31/17.
+//  Created by Ryan Phillips on 8/1/17.
 //  Copyright © 2017 Ryan Phillips. All rights reserved.
 //
 
@@ -16,7 +16,7 @@ extension User {
         return NSFetchRequest<User>(entityName: "User")
     }
 
-    @NSManaged public var id: Int16
+    @NSManaged public var date: NSDate?
     @NSManaged public var units: Int16
     @NSManaged public var weight: Int16
 
