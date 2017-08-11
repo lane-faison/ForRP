87c8a52461946ab611511c4e2dd81bad8a37d879
Ryan Phillips <rphillips4417@gmail.com>
2017-08-11 14:26:54 -0600

    Added functionality to button and attempting to fix addWater function

--- WaterTracker/MainVC.swift
+++ WaterTracker/MainVC.swift
@@ -15,6 +15,8 @@ class MainVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NS
     
     var amounts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128]
     
+    var addedArray: Array<Any> = []
+    
     let backView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -112,6 +114,7 @@ class MainVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NS
         button.setTitleColor(ColorScheme.darkPrimaryColor, for: .normal)
         button.titleLabel?.font = UIFont(name: "Avenir next", size: 15)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addWater), for: .touchUpInside)
         
         return button
     }()
@@ -291,11 +294,8 @@ class MainVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NS
         
     }
     
-//    func addWater() {
-//            
-//            currentLevelLabel.text = amountPicker.selectedRow(inComponent: "\(amounts[row])")
-//    
-//    }
+   
+    
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -311,9 +311,33 @@ class MainVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NS
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        currentLevelLabel.text = "\(amounts[row])"
+        
+        
+//            currentLevelLabel.text = "\(amounts[row])"
         
     }
+    
+    func addWater() {
+        
+//        amountPicker.selectedRow(inComponent: <#T##Int#>)
+        
+    }
+    
+    
+    
+    
+//    func addWater(){
+//        
+//        let row = amountPicker.tag
+//        
+//        if addButton.isSelected {
+//            
+//            amountPicker[row].append
+//            
+//            
+//        }
+//        
+//    }
 
 }
 
